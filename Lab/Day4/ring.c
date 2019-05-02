963a8e4b621ee4559305cc0ecf987c950ebe34a4
nrossi <rossinikolai@gmail.com>
2019-05-02 10:43:26 +0200

    modified ring.c

--- Lab/Day4/ring.c
+++ Lab/Day4/ring.c
@@ -12,26 +12,9 @@ int main(int argc, char* argv[]) {
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
   MPI_Comm_size( MPI_COMM_WORLD, &npes );
 
-  // int i,j=0;
   int x = rank;
   int xlow = 0;
-  // int xright=0;
   int sum=0;
-
-  /*
-  for(i=1; i<=npes; i++) {
-    if(rank==npes-i) MPI_Send(&x, 1, MPI_INT, npes-(i+1), npes-i, MPI_COMM_WORLD);
-    if(rank==npes-(i+1)) {
-      MPI_Recv(&xright, 1, MPI_INT, npes-i, npes-i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      sum += xright;
-      x += sum;
-      printf("after process %d sent %d the values are:\n", npes-i, xright);
-      printf("sum: %d\nx: %d\n", sum, x);
-    }
-  }
-  */
-
-  // if(rank==0) printf("%d\n", sum);
   
   if(rank != 0) {
     MPI_Recv(&xlow, 1, MPI_INT, rank-1, rank-1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
