e00d907bd08406181991d3ca8e0bb88729dff848
nrossi <rossinikolai@gmail.com>
2019-06-06 18:50:39 +0200

    reorg

---
+++ Lab/Assignment_4_5/nonb_identity.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+
+#define N 10  // matrix dimension
+
+void print_matrix(int* A, int rows) {
+  size_t i,j=0;
+  for(i=0; i<rows; i++) {
+    for(j=0; j<N; j++) {
+      fprintf(stdout, "%d\t", *(A+j+i*N));
+    }
+    fprintf(stdout, "\n");
+  }
+}
+
+void swap(int** a, int** b)
+{
+  int* t = *a;
+  *a = *b;
+  *b = t;
+}
+
+
+int main(int argc, char* argv[])
+{
+
+  int rank = 0; // store the MPI identifier of the process
+  int npes = 1; // store the number of MPI processes
+
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+  MPI_Comm_size( MPI_COMM_WORLD, &npes );
+  
+  int N_LOC = N/npes;
+  int REST = N%npes;
+  size_t gap = 0;
+  size_t i,j = 0;
+
+  if(rank < REST) N_LOC++;
+
+  int* mat =(int*)malloc(N*N_LOC*sizeof(int));
+
+  if(rank < REST) {
+    gap = rank*N_LOC;
+  } else {
+    gap = REST*(N_LOC+1)+(rank-REST)*N_LOC;
+  }
+
+  for(i=0; i<N_LOC; i++) {
+    for(j=0; j<N; j++) {
+      if (j == i+gap) *(mat+j+i*N) = 1;
+      else *(mat+j+i*N) = 0;
+    }
+  }
+
+  if(!rank) {
+
+    int* buf = (int*)malloc(N*N_LOC*sizeof(int));
+
+    MPI_Request request;
+
+    if(REST) {
+      for(i=1; i<REST; i++) {
+	MPI_Irecv(buf,N_LOC*N,MPI_INT,i,101,MPI_COMM_WORLD,&request);
+	print_matrix(mat,N_LOC);
+	MPI_Wait(&request,MPI_STATUS_IGNORE);
+	swap(&mat,&buf);
+      }
+
+      MPI_Irecv(buf,(N_LOC-1)*N,MPI_INT,REST,101,MPI_COMM_WORLD,&request);
+      print_matrix(mat,N_LOC);
+      MPI_Wait(&request,MPI_STATUS_IGNORE);
+      swap(&mat,&buf);
+
+      for(i=REST+1; i<npes; i++) {
+	MPI_Irecv(buf,(N_LOC-1)*N,MPI_INT,i,101,MPI_COMM_WORLD,&request);
+	print_matrix(mat,N_LOC-1);
+	MPI_Wait(&request,MPI_STATUS_IGNORE);
+	swap(&mat,&buf);
+      }
+      print_matrix(mat,N_LOC-1);
+    } 
+  
+    else {
+      for(i=1; i<npes; i++) {
+        MPI_Irecv(buf,N_LOC*N,MPI_INT,i,101,MPI_COMM_WORLD,&request);
+        print_matrix(mat,N_LOC);
+	MPI_Wait(&request,MPI_STATUS_IGNORE);
+	swap(&mat,&buf);
+      }
+      print_matrix(mat,N_LOC);
+    }
+
+  }
+
+  else {
+    MPI_Send(mat,N_LOC*N,MPI_INT,0,101,MPI_COMM_WORLD);
+  }
+
+
+  MPI_Finalize();
+  return 0;
+}
